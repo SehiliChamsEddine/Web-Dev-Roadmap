d84ff9196cfec524ba8d1bd08f14465a5e152e2c
Chams Eddine <echams165@gmail.com>
2024-12-29 00:55:58 +0100

    add some adjustments to the ui and some interactivity

--- scripts.js
+++ scripts.js
@@ -13,7 +13,7 @@ messageOpen.addEventListener('click', () => {
 				"Hi there, I am Sehili Chams Eddine, an inspired web developer like you, and I want to welcome you to this simple roadmap, which will be your first step toward becoming a professional web developer. Don't forget to bring coffee and enjoy learning!",
 			],
 			typeSpeed: 75, // Speed of typing
-			backSpeed: 100, // Speed of backspacing
+			backSpeed: 50, // Speed of backspacing
 			backDelay: 1000, // Delay before starting to backspace
 			startDelay: 100, // Delay before starting typing
 			loop: false, // Whether to loop the typing effect
@@ -26,7 +26,7 @@ messageClose.addEventListener('click', () => {
 	messageOpen.classList.remove('remove');
 });
 const st = `<h1>Welcome To <br> The Web <br> Development<br><span>Roadmap</span></h1>
-<h2>Start Your Web Development Journey today</h2>
+<h2>Start Your Web <br> Development Journey <br> today</h2>
 <p>Unlock the tools, technologies, and knowledge to become a web developer.<br> Let’s build your future!</p>`;
 
 const buttonHTML =
@@ -35,7 +35,7 @@ const buttonHTML =
 const typed = new Typed('.typing-text', {
 	strings: [st],
 	typeSpeed: 75, // Speed of typing
-	backSpeed: 100, // Speed of backspacing
+	backSpeed: 50, // Speed of backspacing
 	backDelay: 1000, // Delay before starting to backspace
 	startDelay: 50, // Delay before starting typing
 	loop: false, // Whether to loop the typing effect
